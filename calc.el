f6ba9df1b10477460adbb54d9ed6be287c7cd2be
Shayan Azmoodeh <shkev.az@gmail.com>
2021-11-25 12:35:35 -0800

    adding emacs config files

---
+++ calc.el
@@ -0,0 +1,15 @@
+
+;;; Mode settings stored by Calc on Tue Apr 21 20:16:38 2020
+(setq calc-mode-save-mode 'save)
+(setq calc-matrix-mode 'scalar)
+(setq calc-angle-mode 'rad)
+(setq calc-internal-prec 10)
+(setq calc-language 'math)
+(setq calc-vector-brackets "{}")
+(setq calc-full-vectors nil)
+(setq calc-infinite-mode t)
+(setq calc-complex-format 'I)
+(setq calc-prefer-frac t)
+(setq calc-line-breaking nil)
+;;; End of mode settings
+
